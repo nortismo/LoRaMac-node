af5a35b93b36350ae3cdd6e7e100cd8ebfce1032
Diego Bienz <diego.bienz@toradex.com>
2020-04-16 14:03:56 +0200

    Added comment to delay-board.c in Tiny-Lacuna

--- src/boards/Tiny-Lacuna/delay-board.c
+++ src/boards/Tiny-Lacuna/delay-board.c
@@ -12,11 +12,12 @@
 
 extern volatile uint32_t sysTickCountDown;
 
-void
-DelayMsMcu (uint32_t ms)
-{
-  sysTickCountDown = ms;
-  while (sysTickCountDown != 0U)
-    {
-    }
+/*!
+ * Method makes use of the external sysTickCountDown defined in board.c of Tiny-Lacuna.
+ * As long as the sysTickCountDown is not 0, it will be decremented every millisecond.
+ */
+void DelayMsMcu(uint32_t ms) {
+	sysTickCountDown = ms;
+	while (sysTickCountDown != 0U) {
+	}
 }
