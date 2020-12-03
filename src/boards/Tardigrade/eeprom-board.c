33ebd5bc13c29365a2dab5738599bcaec4fda429
Diego Bienz <diego.bienz@gmail.com>
2020-12-03 11:02:10 +0100

    Added flash driver for nonvolatile memory

--- src/boards/Tardigrade/eeprom-board.c
+++ src/boards/Tardigrade/eeprom-board.c
@@ -8,7 +8,6 @@
  * \author    Diego Bienz
  */
 
-#include <stdio.h>
 #include "utilities.h"
 #include "eeprom-board.h"
 #include "fsl_iap.h"
@@ -17,6 +16,14 @@
 flash_config_t flashInstance;
 bool ErasingOnGoing = false;
 
+/************************************************
+*
+* Total Flash Size: 256 KB (244 KB available for customer)
+* Sector Size: 32 KB
+* Page Size: 512 Bytes
+*
+************************************************/
+
 /*!
  * \brief Initializes the EEPROM emulation module.
  */
@@ -24,7 +31,6 @@ void EepromMcuInit( void ){
 
     if (FLASH_Init(&flashInstance) != kStatus_Success)
     {
-        printf( "Flash wasn't properly initialized\r\n" );
         // Fatal error, endless loop.
         while ( 1 )
         {
@@ -46,31 +52,76 @@ bool EepromMcuIsErasingOnGoing( void )
     return ErasingOnGoing;
 }
 
+/*!
+ * CAUTION: The address and size has to be a multiple of the page size (512 Bytes)
+ */
 uint8_t EepromMcuWriteBuffer( uint16_t addr, uint8_t *buffer, uint16_t size )
 {
-    uint8_t status = FAIL;
+	uint32_t status = FAIL;
+	uint32_t failedAddress, failedData;
 
+	if(size % 512 != 0 || addr % 512 != 0){
+		return FAIL;
+	}
 
+	ErasingOnGoing = true;
+	status = FLASH_Erase(&flashInstance, (uint32_t) addr, (uint32_t) size, kFLASH_ApiEraseKey);
+	ErasingOnGoing = false;
 
-    return status;
+    if(status != kStatus_Success){
+    	return FAIL;
+    }
+
+    status = FLASH_VerifyErase(&flashInstance, (uint32_t) addr, (uint32_t) size);
+
+    if(status != kStatus_Success){
+    	return FAIL;
+    }
+
+    status = FLASH_Program(&flashInstance, (uint32_t) addr, buffer, (uint32_t) size);
+
+    if(status != kStatus_Success){
+    	return FAIL;
+    }
+
+    status = FLASH_VerifyProgram(&flashInstance, (uint32_t) addr, (uint32_t) size, buffer, &failedAddress,
+                                     &failedData);
+
+    if(status != kStatus_Success){
+    	return FAIL;
+    }
+
+    return SUCCESS;
 }
 
+/*!
+ * CAUTION: The address and size has to be a multiple of the page size (512 Bytes)
+ */
 uint8_t EepromMcuReadBuffer( uint16_t addr, uint8_t *buffer, uint16_t size )
 {
-    uint8_t status = FAIL;
+	uint32_t status = FAIL;
 
+	if(size % 512 != 0 || addr % 512 != 0){
+		return FAIL;
+	}
 
+	status = FLASH_Read(&flashInstance, (uint32_t) addr, buffer, (uint32_t) size);
 
-    return status;
+    if(status != kStatus_Success){
+    	return FAIL;
+    }
+
+    return SUCCESS;
 }
 
 void EepromMcuSetDeviceAddr( uint8_t addr )
 {
-    assert_param( FAIL );
+	for(;;){
+		/* This method is not allowed on this platform. */
+	}
 }
 
 uint8_t EepromMcuGetDeviceAddr( void )
 {
-    assert_param( FAIL );
     return FAIL;
 }
